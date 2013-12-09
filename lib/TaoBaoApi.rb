aaccc20776839c263d26cfda787a7e2052b0fa99
wikimo <gwikimo@gmail.com>
2013-12-09 21:26:48 +0800

    init gem

--- lib/TaoBaoApi.rb
+++ lib/TaoBaoApi.rb
@@ -6,7 +6,6 @@ module TaoBaoApi
   class	Good
   	def get_info url
   		return nil if !url
-  		#url = "http://detail.tmall.com/item.htm?id=27318392662&spm=a1z09.5.0.0.fwV7bQ"
 
 		doc = Nokogiri::HTML(open(url)) 
 		doc.encoding = 'utf-8'
@@ -23,21 +22,20 @@ module TaoBaoApi
 			return 'undefined'
 		end
 
-		# p "title -- #{title}  price -- #{price}  img -- #{img}"
 		{:title => title,
 			:price => price,
 			:img => img}
   	end
 
   	private
-		def title_fiter(title)
-			filter_char = ["\n","\r","\t",' ']
+	  def title_fiter(title)
+		filter_char = ["\n","\r","\t",' ']
 
-			filter_char.each do |s|
-				title = title.gsub(s,'')
-			end
-
-			title
+		filter_char.each do |s|
+			title = title.gsub(s,'')
 		end
+
+		title
+	  end
   end	
 end
