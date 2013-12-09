148500a38dc8aeb6f40395abaa78761c5f74881d
wikimo <gwikimo@gmail.com>
2013-12-09 21:24:50 +0800

    init app

---
+++ lib/TaoBaoApi.rb
@@ -0,0 +1,43 @@
+require "TaoBaoApi/version"
+require 'nokogiri'  
+require 'open-uri'
+
+module TaoBaoApi
+  class	Good
+  	def get_info url
+  		return nil if !url
+  		#url = "http://detail.tmall.com/item.htm?id=27318392662&spm=a1z09.5.0.0.fwV7bQ"
+
+		doc = Nokogiri::HTML(open(url)) 
+		doc.encoding = 'utf-8'
+
+		title = title_fiter(doc.css('.tb-detail-hd').first.text.strip)
+
+		if url.include? 'taobao'
+			price = doc.css('em.tb-rmb-num').first.text
+			img = doc.css('#J_ImgBooth').first.attr('data-src')
+		elsif url.include? 'tmall'
+			price = doc.css('.J_originalPrice').first.text.strip
+			img = doc.css('#J_ImgBooth').first.attr('src')
+		else
+			return 'undefined'
+		end
+
+		# p "title -- #{title}  price -- #{price}  img -- #{img}"
+		{:title => title,
+			:price => price,
+			:img => img}
+  	end
+
+  	private
+		def title_fiter(title)
+			filter_char = ["\n","\r","\t",' ']
+
+			filter_char.each do |s|
+				title = title.gsub(s,'')
+			end
+
+			title
+		end
+  end	
+end
---
+++ lib/TaoBaoApi/version.rb
@@ -0,0 +1,3 @@
+module TaoBaoApi
+  VERSION = "0.0.1"
+end
