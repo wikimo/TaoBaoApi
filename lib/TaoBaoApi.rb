c51be2d640c9585def1bc21d197c6f0a4fc33e49
wikimo <gwikimo@gmail.com>
2013-12-10 15:58:48 +0800

    formate code

--- lib/TaoBaoApi.rb
+++ lib/TaoBaoApi.rb
@@ -3,46 +3,46 @@ require 'nokogiri'
 require 'open-uri'
 
 module TaoBaoApi
-	class Good
-		def get_info url
-			return 'good_url_nil' if url.nil?
+  class Good
+    def get_info url
+      return 'good_url_nil' if url.nil?
 
-			doc = Nokogiri::HTML(open(url)) 
-			doc.encoding = 'utf-8'
+      doc = Nokogiri::HTML(open(url)) 
+      doc.encoding = 'utf-8'
 
-			begin
-				title = doc.css('.tb-detail-hd').first.text
-			rescue NoMethodError
-				return 'good_not_exists'
-			end
+      begin
+        title = doc.css('.tb-detail-hd').first.text
+      rescue NoMethodError
+        return 'good_not_exists'
+      end
 
-			title = title_fiter(title.strip)
+      title = title_fiter(title.strip)
 
-			if url.include? 'taobao'
-				price = doc.css('em.tb-rmb-num').first.text
-				img = doc.css('#J_ImgBooth').first.attr('data-src')
-			elsif url.include? 'tmall'
-				price = doc.css('.J_originalPrice').first.text.strip
-				img = doc.css('#J_ImgBooth').first.attr('src')
-			else
-				return 'good_url_is_not_taobao'
-			end
+      if url.include? 'taobao'
+        price = doc.css('em.tb-rmb-num').first.text
+        img = doc.css('#J_ImgBooth').first.attr('data-src')
+      elsif url.include? 'tmall'
+        price = doc.css('.J_originalPrice').first.text.strip
+        img = doc.css('#J_ImgBooth').first.attr('src')
+      else
+        return 'good_url_is_not_taobao'
+      end
 
-			{:title => title,
-				:price => price,
-				:img => img}
-		end
+      {:title => title,
+        :price => price,
+        :img => img}
+    end
 
-		private
-			def title_fiter(title)
-				title = title.split("\r\n")[0]
+    private
+      def title_fiter(title)
+        title = title.split("\r\n")[0]
 
-				filter_char = ["\n","\r","\t",' ']
-				filter_char.each do |s|
-					title = title.gsub(s,'')
-				end
+        filter_char = ["\n","\r","\t",' ']
+        filter_char.each do |s|
+          title = title.gsub(s,'')
+        end
 
-				title
-			end
-	end
+        title
+      end
+  end
 end
